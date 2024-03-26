472d1bcd35e1d29c407367037707460b551e87a1
EvershineTech <165022328+EvershineTech@users.noreply.github.com>
2024-03-26 17:44:45 +0530

    initial commit

---
+++ JenkinsExample.java
@@ -0,0 +1,14 @@
+
+	
+	public class JenkinsExample {
+	
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		
+	System.out.println("Hello Word");
+				
+	System.out.println("Wellcome to Jenkins");
+		
+}
+		
+	}
