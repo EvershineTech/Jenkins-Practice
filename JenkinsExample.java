da1420b230b6d29585859dd192cb28a044682bc8
EvershineTech <165022328+EvershineTech@users.noreply.github.com>
2024-03-26 17:50:00 +0530

    cleared extra space
    extra space cleared

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -5,10 +5,8 @@
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 		
-	System.out.println("Hello Word");
-				
-	System.out.println("Wellcome to Jenkins");
-		
+	System.out.println("Hello Word");				
+	System.out.println("Wellcome to Jenkins");		
 }
 		
 	}
